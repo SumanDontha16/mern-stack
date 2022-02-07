487fba52bef008daff1938b5f1597091d6d23617
Dontha Suman <51446173+SumanDontha16@users.noreply.github.com>
2022-02-07 18:21:20 +0000

    Initial REST API for goals.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleWare')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
+// MongoDB Connection
+connectDB()
 
 const app = express()
 
